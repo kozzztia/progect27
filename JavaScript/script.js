a4ed4547c4794a63faf1d019534fc44fd73f15f1
kozzztia <kozzztia@ukr.net>
2021-11-28 19:41:04 +0200

    task1

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,10 +1,93 @@
-$(document).ready(function(){
+
+// ----------------------------------------------------------------
+
+// let input = document.querySelector('.input');
+// let inputNext = document.querySelector('.input-next');
+// let button = document.querySelector('.button');
+// let title = document.querySelector('h2');
+// // ---------------------
+// button.onclick = function (element, element2) {
+//   element = +input.value;
+//   element2 = +inputNext.value;
+//   let result = element * element2;
+//   // -----------------------
+//   input.value = '';
+//   inputNext.value = '';
+//   title.innerHTML = 'result: ';
+
+//   title.innerHTML += result;
 
 
+// }
+// alert(2019);
+
+// let a = 2019;
+// let b = 200;
+// result = a - b;
+// alert(result)
 
 
+// let one = document.querySelector('#one');
+// one.innerHTML = 'hello World';
+// //
+// let a = 12;
+// let result = a * a;
+// let two = document.querySelector('#two');
+// two.innerHTML = result;
+// //
+// task8 = document.querySelector('.div');
+// task8.innerHTML = 'helloWorld';
+
+// task9 = document.querySelector('span');
+// task9 = task9.innerHTML = 'world';
+
+// task10 = document.querySelector('.three');
+// task10.innerHTML = '<h2> ko ko ko ko!<h2>';
+
+// task11 = document.querySelector('.four');
+// task11.innerHTML = '<h4> article </h4>';
+// task11.innerHTML += '<p> paragraph </p>';
+
+// task12 = document.querySelector('.five');
+// task12.innerHTML = 3.1415;
+
+// console.log(+task12.innerHTML + +task12.innerHTML);
 
 
+// div7 = document.querySelector('.seven');
+// div7.innerHTML = '<img src="https://lh3.googleusercontent.com/WT0DEE1L0IyM8mOMxGw71XzrUtI3bDlS_Hackss_rvbwIOq4jxc6-c3jVcEEQfA6Gjgxppc-EvDF7pf9J2evUCdJphZnAOZ12nruW5aVVu4BxX0lGNgP7KymFuv3iLOr34b1foPHaDE-AbLKT1OTO0MH6-NO-v17kWVm5-RZQZLf9GnZQCE1zcEZUr7pBy9_yNY2pDpmTBuGKkmKPc1n5zgvMQvqkbbOKFE4i4Jla4qGh1iNMoFLPwsrn5uY7xV5dRKhYvuQH1vPQqaugOPd8MHpoXlb4Jg_wEN6PXjFPAgDt9sC_Vt5WSOSINT2XhXKHNhX-K5-B8PxWwvdj8kwsctXZZPnP-aDgwMtSjvDaroM6KCJwz8lZiNZAGSyY6ECnt2Rw9clRVF2oPqZWZyLgtyCmcfBFOZZnHjJH57bS6_cd2DiIU4C91qJxZEDtcq0Msp2dCUgvqJSyH7_ueyFOF5wRSfCO1MyIVvRNWV5gsso7PBtUBJJDhRA7e7KYqkAE4VQns6NdyjTqBpBWnc_krRT_zF32lVTmReYT1Nt1-6vc-iDatiizMWACPW19Jn1sEeO4r1HdwHK2WpTcyPbl1SOVbcVwAD8d1ZzG_fu_es_EoJ0ryR3PMSRqeh2ie1piT326Ok-Yn-S4UJQIhY58Asbrbo7-Yrf3YBovcr1b8MJ7TzobiK6VjxalEsIbqnE0oaIkBm-CAMxpCbLpmubJKs=w720-h557-no?authuser=0" alt="">'
+
+// task14 = document.querySelector('.eight');
+// a1 = 6;
+// a2 = 3;
+// result = a1 * a2;
+// task14.innerHTML = result;
+
+// b1 = 6;
+// b2 = 3;
+
+// resultB = b1 / b2;
+
+// task14.innerHTML = result + " " + resultB;
 
 
-  });
+// task14 = document.querySelector('.eight');
+// x1 = "hello world";
+// x2 = 5;
+// result = x1 + x2;
+// task14.innerHTML = result
+
+// d1 = document.querySelector('.test-1');
+// console.log(d1);
+
+// d2 = document.querySelector('.test-2');
+// console.log(d2);
+// d2 = 4;
+// console.log(d2)
+
+// divS3 = document.querySelector('.s3');
+// console.log(divS3);
+// divS3 = document.querySelector('.s4');
+// console.log(divS3);
+let bo = document.querySelector('body');
+bo.innerHTML = "<h1>ERROR</h1>";  
