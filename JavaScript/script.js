7133a2bcdacbc3c71cb31c8640c461e6a0c98264
kozzztia <kozzztia@ukr.net>
2021-11-29 00:04:24 +0200

    task2

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,93 +1,125 @@
+// --------------------- task2
 
-// ----------------------------------------------------------------
 
+// let a = 7;
+// let b = 9;
+// let result = 7 * 9;
+// console.log(result);
+// let div = document.querySelector('.out');
+// ------
+// let a = 7;
+// let b = 9;
+// let result = a / b;
+// div.innerHTML = result;
+
+
+// let e = 3;
+// let f = 5;
+// let result2 = e + f;
+// div.innerHTML = result2;
+
+// let e1 = '3';
+// let f1 = 5;
+// let result3 = e1 + f1;
+// div.innerHTML = result3;
+// // -----------------string
+// let e2 = 3;
+// let f2 = 0;
+// let result4 = e2 / f2;
+// div.innerHTML = result4;
+// // ----------infinity
+// let e3 = 3;
+// let f3 = 'hello';
+// let result5 = e3 + f3;
+// div.innerHTML = result5;
+// // ---------string
+// let e4 = 3;
+// let f5 = 'hello';
+// let result6 = e3 * f3;
+// div.innerHTML = result6;
+// ---------not a number
+
+// let push = document.querySelector('.btn').onclick = function () {
+//   console.log(document.querySelector('.input').value);
+//   document.querySelector('.input').value = "";
+// }
+// let push = document.querySelector('.btn');
 // let input = document.querySelector('.input');
-// let inputNext = document.querySelector('.input-next');
-// let button = document.querySelector('.button');
-// let title = document.querySelector('h2');
-// // ---------------------
-// button.onclick = function (element, element2) {
-//   element = +input.value;
-//   element2 = +inputNext.value;
-//   let result = element * element2;
-//   // -----------------------
+
+// push.addEventListener('click', (i) => {
+//   i = input.value;
+
+//   // console.log(i);
+//   div.innerHTML = i;
 //   input.value = '';
-//   inputNext.value = '';
-//   title.innerHTML = 'result: ';
 
-//   title.innerHTML += result;
+// })
 
+let push = document.querySelector('.btn');
+let out = document.querySelector('.out');
+let input = document.querySelector('.input');
+let input2 = document.querySelector('.input2');
+
+// push.addEventListener('click', function (e) {
+//   e = input.value;
+//   input.value = '';
+//   out.innerHTML = e * 10;
+
+// })
+
+// push.addEventListener('click', () => {
+//   out.innerHTML = +input.value + 10;
+//   input.value = '';
+// })
+
+// push.addEventListener('click', (value1, value2) => {
+//   value1 = input.value;
+//   value2 = input2.value;
+//   out.innerHTML = 'hello' + ' ' + value1 + ' ' + value2;
+// })
+
+// push.onclick = function (a, b) {
+//   a = +input.value;
+//   b = +input2.value;
+//   input.value = input2.value = '';
+//   console.log(a + b);
+//   out.innerHTML = a + b;
 
 // }
-// alert(2019);
+// push.addEventListener('click', () => {
+//   input.value = 'hello world';
+//   input2.value = 'sobaka sutulaia';
+// })
 
-// let a = 2019;
-// let b = 200;
-// result = a - b;
-// alert(result)
-
-
-// let one = document.querySelector('#one');
-// one.innerHTML = 'hello World';
-// //
-// let a = 12;
-// let result = a * a;
-// let two = document.querySelector('#two');
-// two.innerHTML = result;
-// //
-// task8 = document.querySelector('.div');
-// task8.innerHTML = 'helloWorld';
-
-// task9 = document.querySelector('span');
-// task9 = task9.innerHTML = 'world';
-
-// task10 = document.querySelector('.three');
-// task10.innerHTML = '<h2> ko ko ko ko!<h2>';
-
-// task11 = document.querySelector('.four');
-// task11.innerHTML = '<h4> article </h4>';
-// task11.innerHTML += '<p> paragraph </p>';
-
-// task12 = document.querySelector('.five');
-// task12.innerHTML = 3.1415;
-
-// console.log(+task12.innerHTML + +task12.innerHTML);
+out.style.border = '2px solid green';
 
 
-// div7 = document.querySelector('.seven');
-// div7.innerHTML = '<img src="https://lh3.googleusercontent.com/WT0DEE1L0IyM8mOMxGw71XzrUtI3bDlS_Hackss_rvbwIOq4jxc6-c3jVcEEQfA6Gjgxppc-EvDF7pf9J2evUCdJphZnAOZ12nruW5aVVu4BxX0lGNgP7KymFuv3iLOr34b1foPHaDE-AbLKT1OTO0MH6-NO-v17kWVm5-RZQZLf9GnZQCE1zcEZUr7pBy9_yNY2pDpmTBuGKkmKPc1n5zgvMQvqkbbOKFE4i4Jla4qGh1iNMoFLPwsrn5uY7xV5dRKhYvuQH1vPQqaugOPd8MHpoXlb4Jg_wEN6PXjFPAgDt9sC_Vt5WSOSINT2XhXKHNhX-K5-B8PxWwvdj8kwsctXZZPnP-aDgwMtSjvDaroM6KCJwz8lZiNZAGSyY6ECnt2Rw9clRVF2oPqZWZyLgtyCmcfBFOZZnHjJH57bS6_cd2DiIU4C91qJxZEDtcq0Msp2dCUgvqJSyH7_ueyFOF5wRSfCO1MyIVvRNWV5gsso7PBtUBJJDhRA7e7KYqkAE4VQns6NdyjTqBpBWnc_krRT_zF32lVTmReYT1Nt1-6vc-iDatiizMWACPW19Jn1sEeO4r1HdwHK2WpTcyPbl1SOVbcVwAD8d1ZzG_fu_es_EoJ0ryR3PMSRqeh2ie1piT326Ok-Yn-S4UJQIhY58Asbrbo7-Yrf3YBovcr1b8MJ7TzobiK6VjxalEsIbqnE0oaIkBm-CAMxpCbLpmubJKs=w720-h557-no?authuser=0" alt="">'
 
-// task14 = document.querySelector('.eight');
-// a1 = 6;
-// a2 = 3;
-// result = a1 * a2;
-// task14.innerHTML = result;
+// push.addEventListener('click', (a, b) => {
+//   a = +input.value;
+//   b = +input2.value;
+//   input.value = input2.value = '';
+//   result = a + b
+//   out.innerHTML = result;
+// })
 
-// b1 = 6;
-// b2 = 3;
+push.addEventListener('click', function () {
+  t = +input.value;
+  g = +input2.value;
 
-// resultB = b1 / b2;
+  t = parseInt(t);
+  g = parseInt(g);
 
-// task14.innerHTML = result + " " + resultB;
+  console.log(t + g);
+  out.innerHTML = t + g;
+})
 
+// push.addEventListener('click', () => {
+//   t = input.value;
+//   g = input2.value;
 
-// task14 = document.querySelector('.eight');
-// x1 = "hello world";
-// x2 = 5;
-// result = x1 + x2;
-// task14.innerHTML = result
+//   t = parseFloat(t);
+//   g = parseFloat(g);
+//   console.log(t + ' ' + g)
 
-// d1 = document.querySelector('.test-1');
-// console.log(d1);
-
-// d2 = document.querySelector('.test-2');
-// console.log(d2);
-// d2 = 4;
-// console.log(d2)
-
-// divS3 = document.querySelector('.s3');
-// console.log(divS3);
-// divS3 = document.querySelector('.s4');
-// console.log(divS3);
-let bo = document.querySelector('body');
-bo.innerHTML = "<h1>ERROR</h1>";  
+// })
