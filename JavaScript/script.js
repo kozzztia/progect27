6a24eb0921cf93d3a0e67a5ce9e5650ceecb7b17
kozzztia <kozzztia@ukr.net>
2021-11-30 22:12:45 +0200

    task3

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -55,10 +55,10 @@
 
 // })
 
-let push = document.querySelector('.btn');
-let out = document.querySelector('.out');
-let input = document.querySelector('.input');
-let input2 = document.querySelector('.input2');
+// let push = document.querySelector('.btn');
+// let out = document.querySelector('.out');
+// let input = document.querySelector('.input');
+// let input2 = document.querySelector('.input2');
 
 // push.addEventListener('click', function (e) {
 //   e = input.value;
@@ -91,7 +91,7 @@ let input2 = document.querySelector('.input2');
 //   input2.value = 'sobaka sutulaia';
 // })
 
-out.style.border = '2px solid green';
+// out.style.border = '2px solid green';
 
 
 
@@ -103,16 +103,16 @@ out.style.border = '2px solid green';
 //   out.innerHTML = result;
 // })
 
-push.addEventListener('click', function () {
-  t = +input.value;
-  g = +input2.value;
+// push.addEventListener('click', function () {
+//   t = +input.value;
+//   g = +input2.value;
 
-  t = parseInt(t);
-  g = parseInt(g);
+//   t = parseInt(t);
+//   g = parseInt(g);
 
-  console.log(t + g);
-  out.innerHTML = t + g;
-})
+//   console.log(t + g);
+//   out.innerHTML = t + g;
+// })
 
 // push.addEventListener('click', () => {
 //   t = input.value;
@@ -123,3 +123,91 @@ push.addEventListener('click', function () {
 //   console.log(t + ' ' + g)
 
 // })
+// -----------------------------------------------------------------------------------------------------
+// let push = document.querySelector('.btn');
+// let out = document.querySelector('.out');
+// let input = document.querySelector('.input');
+// let input2 = document.querySelector('.input2');
+// -----------------------------------------------------------------------------------------------------
+
+// task 1
+// const btn1 = document.querySelector('.btn1');
+// const btn2 = document.querySelector('.btn2');
+// const div = document.querySelector('div');
+// const input = document.querySelector('.input');
+
+// btn1.addEventListener('click', () => {
+//   let num = +input.value;
+
+//   if (num <= 18 && num > -1) {
+//     div.innerHTML = 'to yong';
+//   }
+//   else if (num > 18 && num <= 90) {
+//     div.innerHTML = 'you welcome';
+//   }
+//   else if (num > 90) {
+//     div.innerHTML = 'sorry you to old';
+//   }
+//   else {
+//     div.innerHTML = 'not correct input';
+//   }
+
+//   switch (num) {
+//     case 18:
+//       console.log("немножко");
+//       break;
+//     case 19:
+//       console.log("поехали");
+//       break;
+//     case 20:
+//       console.log("позновато");
+//       break;
+//     default:
+//       console.log("втираеш куюто дичь");
+//       break;
+
+//   }
+// })
+
+
+// ---------------section 3 ---------------------------
+const btn1 = document.querySelector('.btn1');
+const btn2 = document.querySelector('.btn2');
+const div = document.querySelector('.div');
+// const input1 = document.querySelector('.input1');
+// const input2 = document.querySelector('.input2');
+// task 1----------------------------------------
+// let f1 = function () {
+//   i = input.value;
+//   div.innerHTML = i == 4;
+// }
+// btn1.addEventListener('click', f1);
+// task 2----------------------------------
+// let f2 = function () {
+//   a = input1.value;
+//   b = input2.value;
+//   if (a < b) {
+//     div.innerHTML = a + ' меньше ' + b;
+//   }
+//   else if (a > b) {
+//     div.innerHTML = b + ' больше ' + a;
+//   }
+//   else {
+//     div.innerHTML = a + ' ровно ' + b;
+//   }
+// }
+// btn1.addEventListener('click', f2);
+// task 3-------------------------------------------
+
+let f3 = function () {
+  let a = document.querySelector('.input1').value;
+  let b = document.querySelector('.input2').value;
+  if (a < b) {
+    div.innerHTML = b;
+  }
+  else {
+    div.innerHTML = a;
+  }
+}
+btn1.addEventListener('click', f3)
+
