2d8a2f7bbc4b7521819340b2280e7ad1095fa5b6
Kozzztia <kozzztia@ukr.net>
2021-12-23 23:50:52 +0200

    weather/2

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,21 +5,22 @@ let cityName = document.querySelector('.cityName');
 let price = document.querySelector('.price');
 let clouds = document.querySelector('.clouds');
 let icon = document.querySelector('.icon');
+let winds = document.querySelector('.winds');
+
 
 push.onclick = function () {
     let town = input.value;
     console.log(town)
-    fetch(`http:/api.openweathermap.org/data/2.5/weather?q=${town},ua&appid=b10a1709279e52171ae6535fa8b02312`)
+    fetch(`https:/api.openweathermap.org/data/2.5/weather?q=${town},ua&appid=b10a1709279e52171ae6535fa8b02312`)
         .then(function (resp) { return resp.json() })
         .then(function (data) {
             console.log(data);
             cityName.innerHTML = data.name;
-            price.innerHTML = `temperature = ${data.main.temp.toPrecision(2) - 273} &deg`;
+            price.innerHTML = `temperature = ${data.main.temp.toPrecision(3) - 273} &deg`;
             clouds.textContent = `weather = ${data.weather[0].description}`;
             let iconNumber = data.weather[0].icon;
-            console.log(iconNumber)
-            icon.innerHTML = `<img src = https:/openweathermap.org/img/wn/${iconNumber}@2x.png alt = "img">`;
-
+            icon.innerHTML = `<img src = https:/openweathermap.org/img/wn/${iconNumber}@2x.png alt = "img"> `;
+            winds.innerHTML = `wind speed = ${data.wind['speed']} m/s`;
         })
         .catch(function () {
 
